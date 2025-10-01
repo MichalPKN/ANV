6e69628ee02ad151da6a9c3c2f60d1e584ff4dc4
Michal_Pokorny <michal.pokorny@tul.cz>
2025-10-01 11:34:25 +0200

    cv1 test

---
+++ ANV/src/test/java/cv1/CafeConfigTest.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package cv1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author michal.pokorny
+ */
+public class CafeConfigTest {
+    @Test
+    public void testSingleton() {
+        CafeConfig cafe = CafeConfig.getInstance("kavarna1");
+        CafeConfig secondCafe = CafeConfig.getInstance("cafe2");
+        System.out.println(cafe.getCafeName());
+        System.out.println(secondCafe.getCafeName());
+        System.out.println(CafeConfig.getInstance("cukrarna3").getCafeName());
+
+        assertEquals("kavarna1", cafe.getCafeName());
+        assertEquals("kavarna1", secondCafe.getCafeName());
+        assertEquals("kavarna1", CafeConfig.getInstance("cukrarna3").getCafeName());
+    }
+    
+}
